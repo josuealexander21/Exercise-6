f6c2e68235e2715c03a4b9c342267f88106b8d9b
josuealexander21 <jdiazloz@terpmail.umd.edu>
2024-10-15 19:48:38 -0400

    created a function that checks whether the number is even or odd

--- exercise_6.py
+++ exercise_6.py
@@ -1,2 +1,11 @@
-print("hello!")
-print("goodbye")
+# random code for exercise 6 Github
+
+def check_number(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+number = 21
+result = check_number(number)
+print(f"{number} is {result}")
