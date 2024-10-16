cd4f7fc38beba44f4aa83ac6e6e24c5d82cb0c74
“Princess <princessfianko@yahoo.com>
2024-10-15 20:39:59 -0400

    exercise_6.py
    Changed number for further testing

--- exercise_6.py
+++ exercise_6.py
@@ -1,11 +1,11 @@
-# random code for exercise 6 Github
-
+print("hello!")
+print("goodbye")
 def check_number(number):
     if number % 2 == 0:
         return "Even"
     else:
         return "Odd"
-
-number = 21
+    
+number = 12
 result = check_number(number)
 print(f"{number} is {result}")
